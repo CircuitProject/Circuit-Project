e0ff5e466fab3f1999c85ce8a27b410336b9e399
afghany <knashanas@gmail.com>
2019-08-05 01:29:07 +0200

    [Travis] Log more info

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -16,7 +16,7 @@ fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 ${TEST_RUNNER_EXTRA} --quiet
+  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 ${TEST_RUNNER_EXTRA}
   END_FOLD
 fi
 
